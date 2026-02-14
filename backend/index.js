a857c5c9e7434f8fc47e1d656ef89a8213e888eb
unknown <helloiampallavi@gmail.com>
2026-02-14 17:44:06 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
-
-import express from 'express'
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
